8bbc000fab55cafc5b5f8ed8d200819b20ab2302
samvitakarkal <samvitaskarkal@gmail.com>
2024-07-18 12:44:37 +0530

    [10] Added customer email validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
+  has_many :order_products
+  
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :customer_email, format: {with: VALID_EMAIL_REGEX}, allow_nil: true
+
   enum status: { pending: 0, placed: 1, shipped: 2, completed: 3, cancelled: 4 }
 end
