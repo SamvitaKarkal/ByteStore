f370e61e19e3e99172a47687fa5cf6c758562b27
samvitakarkal <samvitaskarkal@gmail.com>
2024-07-19 00:37:51 +0530

    [10] Added chart to dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :stocks
+  has_many :order_products
 end
--- steps.rb
+++ steps.rb
@@ -2,3 +2,10 @@
 # to generate product: bin/rails g scaffold admin::product name:string description:text price:integer category:references active:boolean
 # rails g scaffold admin::stock product:references amount:integer size:string
 # bin/rails g scaffold admin::order customer_email:string status:integer total:integer address:string
+# 
+# rails generate config:install
+# bin/importmap pin chart.js
+# bin/rails g stimulus dashboard
+# if facing error with charts.js:
+# 1. run bundle install  ./bin/rails assets:precompile   ./bin/rails assets:clean
+# 2. run bin/importmap pin chart.js and fix imports
